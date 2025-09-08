aedd4f6314325031f9005e5e7d76896fd08a49ba
liliaser8-byte <liliaser8@gmail.com>
2025-09-08 11:55:01 +0300

    Add project files.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath> 
+#include <windows.h> 
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cout << "Введіть сторони трикутника a, b, c: ";
+    cin >> a >> b >> c;
+
+    double p = (a + b + c) / 2.0;
+    double S = sqrt(p * (p - a) * (p - b) * (p - c));
+    double r = S / p;
+
+    cout << "Радіус вписаного кола: " << r << endl;
+
+    return 0;
+}
